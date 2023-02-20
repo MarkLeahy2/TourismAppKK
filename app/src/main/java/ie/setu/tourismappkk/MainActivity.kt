42a572c56d8fc932adb2ee0b54275a291731c923
MarkLeahy2 <82410431+MarkLeahy2@users.noreply.github.com>
2023-02-20 22:12:47 +0000

    Login button added to App

---
+++ app/src/main/java/ie/setu/tourismappkk/MainActivity.kt
@@ -0,0 +1,19 @@
+package ie.setu.tourismappkk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun clickMe(v : View) {
+        Toast.makeText(this,
+            "Logging in",
+            Toast.LENGTH_LONG).show()
+    }
+
+}
