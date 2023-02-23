cffcfcf3faa5a27d1cefa5d32179d860830abf19
MarkLeahy2 <82410431+MarkLeahy2@users.noreply.github.com>
2023-02-23 22:39:43 +0000

    menu_main.xml file created & Menu source method added

--- app/src/main/java/ie/setu/tourismappkk/activities/TourismListActivity.kt
+++ app/src/main/java/ie/setu/tourismappkk/activities/TourismListActivity.kt
@@ -2,6 +2,7 @@ package ie.setu.tourismappkk.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import ie.setu.tourismappkk.R
 
 
@@ -12,5 +13,9 @@ class TourismListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tourism_list)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
